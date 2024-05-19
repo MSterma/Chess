f9bb7d2fabf3b1c770f247177717711a5fabba04
MSterma <mateusz.sterma0703@gmail.com>
2024-05-19 22:37:42 +0200

    Dodaj pliki projektów.

---
+++ hello.cpp
@@ -0,0 +1,40 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+
+int main()
+{
+    al_init();
+    al_install_keyboard();
+    al_install_mouse();
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    ALLEGRO_DISPLAY* disp = al_create_display(320, 200);
+    ALLEGRO_FONT* font = al_create_builtin_font();
+    
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_mouse_event_source());
+    bool redraw = true;
+
+
+    al_start_timer(timer);
+    while (redraw)
+    {
+        al_flip_display();    
+       ALLEGRO_EVENT event;
+       al_wait_for_event(queue, &event);
+       if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+           redraw = false;
+       }
+ 
+    }
+
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
